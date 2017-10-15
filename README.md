83677dbb1a8e3a9b337ce6e20dcad7322e682e6f
Manuel Pasieka <manuel.pasieka@csf.ac.at>
2017-10-15 18:26:00 +0200

    Finishing first level, adding README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Breakout - A simple godot game
+
+This is a very simple [https://en.wikipedia.org/wiki/Breakout_(video_game)](breakout) clone build with the [https://godotengine.org](godot) engine.
+
+![Breakout](https://github.com/mapa17/Breakout/Images/Screenshot.png)
--- Scripts/Duplication_Brick.gd
+++ Scripts/Duplication_Brick.gd
@@ -2,18 +2,27 @@ extends StaticBody2D
 
 export var VALUE = 5
 export var SOUND = 'duplication_brick'
+export var DAMAGE = 1.0
+var current_damage = 0.0
 
 func _ready():
 	pass
 
-func ball_contact(ball):
+func _destruction():
 	get_node('/root/World/Sound').play(SOUND, true)
 	queue_free()
-	get_node('/root/World').score += VALUE
+	get_node('/root/World').score += VALUE	
 	get_parent().free_brick()
-	
+
+func ball_contact(ball):
 	# Spawn a new ball at the position of this brick going into a random direction
 	var new_ball = ball.duplicate()
-	new_ball.set_pos(	get_global_pos())
-	new_ball.set_linear_velocity(Vector2((randf()-0.5)*500, -200))
+	new_ball.set_pos(get_global_pos())
+	var new_velocity = Vector2((randf()-0.5)*500, -200)
+	new_ball.set_linear_velocity(new_velocity.normalized() * ball.get_linear_velocity().length())
 	get_node('/root/World').add_child(new_ball)
+
+	current_damage += DAMAGE
+		
+	if current_damage >= 1.0:
+		_destruction()
